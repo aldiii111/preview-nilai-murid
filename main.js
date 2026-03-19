23b9ebeff303acaa31d7f4f34ceae49ef8b7f5f9
aldi <aldinissa093@gmail.com>
2026-03-19 15:54:57 +0700

    fix: add array

---
+++ main.js
@@ -0,0 +1,62 @@
+const dataDB = [
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "tidak lulus",
+        nilai: 48
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "lulus",
+        nilai: 86
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "lulus",
+        nilai: 92
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "lulus",
+        nilai: 78
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "tidak lulus",
+        nilai: 24
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "lulus",
+        nilai: 100
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "lulus",
+        nilai: 73
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "tidak lulus",
+        nilai: 69
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "lulus",
+        nilai: 87
+    },
+    {
+        nama: "bagus",
+        kelas: "IX",
+        keterangan: "tidak lulus",
+        nilai: 55
+    },
+]
