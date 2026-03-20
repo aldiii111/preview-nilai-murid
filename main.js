e34c599568e29c5b7be80703be01b8a2aabb1192
aldi <aldinissa093@gmail.com>
2026-03-20 12:52:05 +0700

    fix: result err

--- main.js
+++ main.js
@@ -66,7 +66,6 @@ const dataDB = [
 const div = document.createElement("div");
 div.style.boxShadow = "0 2px 5px 2px rgb(0, 0, 0, 0.2)";
 div.style.padding = "10px";
-div.style.display = "flex";
 div.style.flexDirection = "column";
 const b = document.createElement("b");
 
@@ -103,13 +102,25 @@ formSearch.addEventListener("submit", function (event) {
         manippp[0];
     }
     if (inputUser === "riski") {
-      sectionView.append();
-      b.innerHTML = "";
-      // card()
+      card();
+      b.innerHTML =
+        "nama : " +
+        inputUser +
+        "<br><br>" +
+        "kelas : IX" +
+        "<br>" +
+        "keterangan : " +
+        manipp[1] +
+        "<br>" +
+        "nilai : " +
+        manippp[1];
     }
-    console.log(inputUser);
+    div.style.display = "flex";
+    alertWrong.textContent = "";
   } else {
     alertWrong.textContent = "harap isi nama murid yg ada!!!";
     alertWrong.style.color = "red";
+    b.innerHTML = ""
+    div.style.display = "none"
   }
 });
