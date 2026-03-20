31e2924f3da9995fa49e3276e27c4eaac4a99c93
aldi <aldinissa093@gmail.com>
2026-03-20 12:25:08 +0700

    fixing

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const sectionView = document.getElementById("sectionView");
+
 const dataDB = [
   {
     nama: "bagus",
@@ -61,19 +63,38 @@ const dataDB = [
   },
 ];
 
+const div = document.createElement("div");
+div.style.boxShadow = "0 2px 5px 2px rgb(0, 0, 0, 0.2)";
+div.style.padding = "10px";
+div.style.display = "flex";
+div.style.flexDirection = "column";
+const b = document.createElement("b");
+
 const formSearch = document.getElementById("formSearch");
 formSearch.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const inputUser = document.getElementById("inputSearch").value;
-  const alertWrong = document.getElementById("alertWrong")
+  const alertWrong = document.getElementById("alertWrong");
 
-  const includs = dataDB.map((nama) => nama.nama).includes(inputUser)
+  function card() {
+    div.append(b);
+    sectionView.append(div);
+  }
 
-  if(includs) {
-    alert('ril cuy')
-  }else{
-    alertWrong.textContent = "harap isi nama murid !!!"
-    alertWrong.style.color = "red"
+  const manip = dataDB.map((namaMurid) => namaMurid.nama);
+  const manipp = dataDB.map((ket) => ket.keterangan)
+  const manippp = dataDB.map((nilai) => nilai.nilai)
+  console.log(manip, manipp, manippp)
+
+  if (manip.includes(inputUser)) {
+    if (inputUser === "bagus") {
+      card();
+      b.innerHTML = "nama : " + inputUser + "<br><br>" + "kelas : IX" + "<br>" + "keterangan : " + manipp[0] + "<br>" + "nilai : " + manippp[0];
+    }
+    console.log(inputUser);
+  } else {
+    alertWrong.textContent = "harap isi nama murid yg ada!!!";
+    alertWrong.style.color = "red";
   }
 });
